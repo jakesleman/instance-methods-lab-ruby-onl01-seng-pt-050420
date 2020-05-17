9d1f1a0593476ef3d0e6e202d40af15be3c65144
Jake Sleman <jakesleman@icloud.com>
2020-05-17 21:12:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
+  
 class Dog
-
-def bark 
-  puts "Woof!"
   
+  def bark
+    puts "Woof!"
+  end
   
-  fido = Dog.new
-  fido.bark 
-
+  def sit 
+    puts "The Dog is sitting"
+  end
+  
 end
-end 
-
